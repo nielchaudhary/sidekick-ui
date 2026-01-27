93fb5264527c8b704ee09c644e644752bcacb2d3
neel c <neilchaudhary12@gmail.com>
2026-01-27 12:27:10 +0530

    feat: bg blcak

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,11 @@ import FeatureSwitcher from "./components/FeatureSwitcher";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full relative">
-      {/* Crimson Depth Background */}
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          background: "radial-gradient(125% 125% at 50% 10%, #000000 40%, #2b0707 100%)",
-        }}
-      />
-      <div className="relative z-10">
-        <Navbar />
-        <NexusHero />
-        <div className="max-w-8xl mx-auto">
-          <FeatureSwitcher />
-        </div>
+    <main className="min-h-screen w-full bg-black">
+      <Navbar />
+      <NexusHero />
+      <div className="max-w-8xl mx-auto">
+        <FeatureSwitcher />
       </div>
     </main>
   );
